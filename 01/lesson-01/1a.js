f6d68c9dc15bf7eefd800f631b03d0647114dc9c
muhammedtahir1 <mohammedtahir65080@gmail.com>
2023-12-09 12:21:08 +0530

    added 3 lessons

---
+++ 01/lesson-01/1a.js
@@ -0,0 +1,10 @@
+// alert("KingTahir");
+
+// document.body.innerHTML = "Good Morning";
+
+// x = 100 - 20 - 50 + 200;
+// console.log(x)
+
+// to make the web page blank
+// document.body.innerHTML = "";
+
---
+++ 01/lesson-02/2a.js
@@ -0,0 +1,31 @@
+// x = (18.50 + 2*7.50) * (20/100)
+// console.log(x);
+
+
+
+y = Math.round((2095 + 799 + 499) * 0.1) / 100;
+// console.log(y);
+
+total = (2095 + 799 + 499) / 100 + Math.round((2095 + 799 + 499) * 0.1) / 100;
+// console.log(total);
+
+Math.floor(2.8);   // output: 2
+Math.ceil(2.2);    // output : 3
+
+// Temperature
+let fahrenheit = 86;
+let celsius = 25;
+
+let temp = (celsius * 9/5) + 32;
+// console.log(temp);
+
+let temp1 = (fahrenheit - 32) * 5/9;
+// console.log(temp1);
+
+
+
+
+
+
+
+
